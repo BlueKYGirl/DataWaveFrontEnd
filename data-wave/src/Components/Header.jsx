c4ab7d1969b2086b27d76a59c302b43ecbfdb888
Toni Moore <toni.denise.moore@gmail.com>
2024-04-12 17:40:06 -0400

    Started front end app

---
+++ data-wave/src/Components/Header.jsx
@@ -0,0 +1,18 @@
+ import React from 'react';
+ export class Header extends React.Component {
+    //componentDidMount(): void {
+         // Normally used to make API calls or feth data
+    //}
+    //componentWillUnmount(): void {
+         // Used to clean up the component
+    render() {
+         return (
+             <div>
+                <nav>
+                <h1>Header</h1>
+                </nav>
+                 
+             </div>
+         )
+    }
+ }
