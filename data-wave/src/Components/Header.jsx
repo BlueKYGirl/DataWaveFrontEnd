94a0f800c2f5240fd0903004d558b1d6f865944f
Toni Moore <toni.denise.moore@gmail.com>
2024-04-15 18:02:45 -0400

    Header, footer drafts

--- data-wave/src/Components/Header.jsx
+++ data-wave/src/Components/Header.jsx
@@ -1,17 +1,26 @@
  import React from 'react';
  export class Header extends React.Component {
-    //componentDidMount(): void {
-         // Normally used to make API calls or feth data
-    //}
-    //componentWillUnmount(): void {
-         // Used to clean up the component
+          userName = "Hunter Atkins";
+         
     render() {
          return (
              <div>
+               
+               <header>
+               <img src="../../full_logo_trans.png" />
                 <nav>
-                <h1>Header</h1>
+                    
+                
+                <div className='accountMenu'>
+                <h2>Account</h2></div>
+                <div className='deviceMenu'>
+                <h2>Devices</h2></div>
+                
+                <div className='login'>
+                <h2>Sign In</h2>
+                </div>
                 </nav>
-                 
+                </header>
              </div>
          )
     }
--- data-wave/src/pages/Home.jsx
+++ data-wave/src/pages/Home.jsx
@@ -1,1 +1,25 @@
- 
+ import React, {useContext, useEffect} from "react";
+ import {Header } from "../Components/Header";
+ import {Footer } from "../Components/Footer";
+
+ import '../styles.css';
+
+ export function Home() {
+
+    const [count, setCount] = React.useState(0);
+     React.useEffect(() => {
+         console.log("Component mounted");
+         return () => {
+            console.log("Component unmounted");
+         };
+        }, []);
+
+        return (
+         <>
+             <Header />
+             <h1>Home</h1>
+         
+         <Footer />
+         </>
+     );
+ }
